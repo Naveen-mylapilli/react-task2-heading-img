cbd60a55fc9efbd453d6d092a7d1732fea96e890
Naveen-mylapilli <naveen.mylapilli08@gmail.com>
2024-11-19 23:53:48 +0530

    Add files via upload

---
+++ second-react-class/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hiii All This My Second React Code With Image!
+      </h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8wh_vrysQFY_0CkcD-e-lYPPHLvsTaXqIGA&s" ></img>
+    </div>
+  );
+}
+
+export default App;
